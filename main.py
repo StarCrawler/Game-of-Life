5aa55a1708bd43130ee4789751b0a42f67f04ec7
StarCrawler <riwanb31@gmail.com>
2021-11-15 16:14:10 +0100

    rename fonction and class files

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import sys
 import pygame as pg
-import fonction
-#import class
+import Game_of_Life_Fonction as fct
+import Game_of_Life_Class
 import numpy as np
 import time
 
 pg.init()
 
 displaySize = width, height = 720, 576
+maxPrintGrid = [width, height - 100]
 xPosGridLine = 0
 yPosGridLine = 0
 cellSize = 10
-gridSize = [int(height/cellSize), int(width/cellSize)]
+gridSize = [int(maxPrintGrid[1]/cellSize), int(width/cellSize)]
 grid = np.zeros(gridSize)
 
 
@@ -57,9 +58,9 @@ grid[4][36] = 1
 #print(grid)
 
 while True:
-    fonction.printCell(grid, gridSize, screen, cellSize)
-    fonction.grid(displaySize,xPosGridLine, yPosGridLine ,cellSize,screen)
-    fonction.cell(grid)
+    fct.printCell(grid, gridSize, screen, cellSize)
+    fct.grid(maxPrintGrid,xPosGridLine, yPosGridLine ,cellSize,screen)
+    fct.cell(grid)
     pg.display.flip()
     for event in pg.event.get():
         if event.type == pg.QUIT:
