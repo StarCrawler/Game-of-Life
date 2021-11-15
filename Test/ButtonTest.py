c79fd24ed7207f295f5d8c55d5adebc12b15a1fd
StarCrawler <riwanb31@gmail.com>
2021-11-15 13:01:33 +0100

    add collide/click on button class

--- Test/ButtonTest.py
+++ Test/ButtonTest.py
@@ -4,6 +4,7 @@ import sys
 pg.font.init()
 displaySize = width, height = 720, 576
 screen = pg.display.set_mode(displaySize)
+screenColor = pg.Color(255,255,255)
 
 class Button(object):
     """docstring for Button."""
@@ -25,12 +26,19 @@ class Button(object):
         pg.draw.rect(self.hisDisplay, self.hisColor, pg.Rect(self.hisX, self.hisY, self.hisWidth, self.hisHeight))
         self.hisDisplay.blit(text,(200,100+(50-10)))
 
+    def isCollided(self,Pos):
+        if Pos[0] >= self.hisX and Pos[0] <= self.hisX + self.hisWidth and Pos[1] >= self.hisY and Pos[1] <= self.hisY + self.hisHeight:
+            return True
+
     def isClicked(self):
+        if pg.mouse.get_pressed()[0] and self.isCollided(pg.mouse.get_pos()):
+            screen.fill(pg.Color(0,0,0))
 
 testButton = Button("Test", 200, 100, pg.Color(100, 100, 100), 100, 100, screen )
 while 1:
-    screen.fill(pg.Color(255, 255, 255))
+    screen.fill(pg.Color(255,255,255))
     testButton.printButton()
+    testButton.isClicked()
     pg.display.flip()
     for event in pg.event.get():
         if event.type == pg.QUIT:
